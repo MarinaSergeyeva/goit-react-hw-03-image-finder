ee2f70c36ec21d6c12e071a65d7f8908690edbaa
Marina Sergeyeva <morrrinad@gmail.com>
2020-08-20 14:07:35 +0300

    Updates

---
+++ precache-manifest.e8bd06ec7d9938b14d683fa82de9912e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c4c90c8af575fcde44d241ed0e4990",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c7158db297b9747889b9",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.1c805611.chunk.css"
+  },
+  {
+    "revision": "bab7a56ed4170dec11a7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6e0fa2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6e0fa2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7158db297b9747889b9",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.71b48c9a.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
